32000a11bf9964a384442d2874d6a2f99723e6f2
Luis <luis.moreno-a@mail.escuelaing.edu.co>
2019-10-11 22:48:31 -0500

    ~

--- src/TestSimulations.py
+++ src/TestSimulations.py
@@ -26,7 +26,7 @@ class TestSimulatons(unittest.TestCase):
             [3],
             [10]
         ]
-        result = Simulations.classicDinamicSystem(state,graph,1)
+        result = Simulations.classicDinamicSystem(state, graph, 1)
         s = [
             [0],
             [0],
@@ -36,15 +36,31 @@ class TestSimulatons(unittest.TestCase):
             [9]
         ]
         for i in range(len(result)):
-            self.assertEqual(result[i][0],s[i][0]);
+            self.assertEqual(result[i][0], s[i][0])
 
     # Rendijas clasico con probabilidaades
-    def test_rendijasProbabilistico(self):
-        r= Simulations.nSlit(2,5,3)
-        res=[0.0, 0.0, 0.0, 0.1665, 0.1665, 0.333, 0.1665, 0.1665]
-        self.assertEqual(r,res)
+    # def test_rendijasProbabilistico(self):
+    #     r = Simulations.nSlit(2, 5, 3)
+    #     res = [0.0, 0.0, 0.0, 0.1665, 0.1665, 0.333, 0.1665, 0.1665]
+    #     self.assertEqual(r, res)
+
+    # Sistema cuántico de partícula en una línea
+
+    def test_probabilidad(self):
+        ket = [
+            [comp.Complex(-3, -1)],
+            [comp.Complex(0, -2)],
+            [comp.Complex(0, 1)],
+            [comp.Complex(2,0)]
+        ]
+        i = 2
+        r = Simulations.probability(ket, i)
+        s =  0.052624
+        self.assertAlmostEqual(r,s)
+
 
     # Otras Pruebas
+
     def test_ProbAndTransition(self):
         pass
 
